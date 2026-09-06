7d5df5c01787d34e22ed0c778ac0fa6d3bd23e0b
aghiyaghaniiy <ghaniyalma@gmail.com>
2026-09-06 14:52:33 +0700

    git pull

---
+++ suki_karedok.php
@@ -0,0 +1,32 @@
+<?php include 'koneksi.php'; ?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Varian Spesial - Seblak Gahoel</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="logo.jpg" alt="Logo Seblak Gahoel">
+        <h1>Seblak Gahoel</h1>
+    </header>
+    <nav>
+        <a href="index.php">Beranda</a>
+        <a href="menu.php">Menu & Varian</a>
+        <a href="pesan.php">Pesan Sekarang</a>
+    </nav>
+    <div class="container">
+        <div class="card">
+            <h3>Detail Kategori Varian Suki, Seafood, Karedok, & Karamel</h3>
+            <p>Berikut adalah rincian varian unggulan yang paling dicari:</p>
+            <ul>
+                <li><b>Varian Seafood:</b> Kerang Ijo, Kerang Dara, Cumi, Udang.</li>
+                <li><b>Varian Suki:</b> Dumpling Ayam, Dumpling Keju, Shrimp, Twister, Cihua.</li>
+                <li><b>Varian Karedok:</b> Karedok Basreng, Cireng Sweet, Cipuk, Cilok, Tahu Putih, Mix 2-5 Macam, Seblak Jadoel, Seblak Rafael.</li>
+                <li><b>Varian Karamel:</b> Karamel Basreng, Cireng Sweet, Cilok, Tahu Putih, Mix 2-4 Macam, Ceker, Tulang.</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
